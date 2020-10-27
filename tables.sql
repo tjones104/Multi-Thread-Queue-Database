957998c7f552385bcc8291a628a613b7a80dd901
Tristan Jones <tristankylerjones@gmail.com>
2020-10-27 13:56:23 -0500

    Test file to look at tables

---
+++ tables.sql
@@ -0,0 +1,15 @@
+-- Third, update seats_booked and seats_available in the flights table
+SELECT * 
+FROM flights;
+
+-- Second, generate ticket number and insert into tickets and ticket_flights if there are available seats in the specified flight and the date  
+SELECT * 
+FROM ticket;
+
+SELECT * 
+FROM ticket_flights;
+
+-- First, generate book_ref number for bookings
+SELECT * 
+FROM bookings;
+
